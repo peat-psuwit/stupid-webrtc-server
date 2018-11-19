d2821e7a06ce0bd7adcf8a318da891fed87266b5
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-11-20 00:58:47 +0700

    Add server implementation

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
+
+const socketio = require('socket.io');
 
 const basePath = `${__dirname}/public`;
 
@@ -17,6 +20,65 @@ const httpServer = http.createServer(function(req, res) {
         res.end();
     });
     stream.pipe(res);
+});
+
+const socketMap = {};
+
+const io = socketio(httpServer);
+
+io.on('connection', (socket) => {
+  let selfId;
+  do {
+    selfId = crypto.randomBytes(6).toString('base64');
+  } while (socketMap[selfId]);
+
+  socketMap[selfId] = socket;
+  socket.emit('connected', { selfId });
+  console.log(`Connected: ${selfId}`);
+
+  socket.on('disconnect', () => {
+    console.log(`Disconnected: ${selfId}`);
+    socket[selfId] = undefined;
+  });
+
+  socket.on('call', ({ calleeId, sdp }) => {
+    console.log(`${selfId} call ${calleeId}`);
+
+    const callee = socketMap[calleeId];
+
+    if (!callee) {
+      socket.emit('failure', { error: 'No callee' });
+      return;
+    }
+
+    callee.emit('ring', { callerId: selfId, sdp });
+  });
+
+  socket.on('answer', ({ callerId, sdp }) => {
+    console.log(`${selfId} answer ${callerId}`);
+
+    const caller = socketMap[callerId];
+
+    if (!caller) {
+      socket.emit('failure', { error: 'No caller' });
+      return;
+    }
+
+    caller.emit('answer', { calleeId: selfId, sdp });
+  });
+
+  socket.on('ice', ({ peerId, candidate }) => {
+    console.log(`${selfId} ice ${peerId}`);
+
+    const peer = socketMap[peerId];
+
+    if (!peer) {
+      socket.emit('failure', { error: 'No peer' });
+      return;
+    }
+
+    peer.emit('ice', { callerId: selfId, candidate });
+  });
 });
 
 const port = parseInt(process.env.PORT) || 8080;
